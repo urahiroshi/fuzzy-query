bc4a4fa8458b0b85e88ed13dcc40d9aafb504271
urahiroshi <asj.hiroshi@gmail.com>
2017-03-06 09:44:02 +0900

    Grouping tests

--- test.js
+++ test.js
@@ -1,70 +1,82 @@
-describe('template', function() {
+describe('fuzzy-query', function() {
   beforeAll(function () {
     document.body.innerHTML = __html__['test.html'];
   })
 
-  it('should select by RegExp Selector', function () {
-    expect(FQ(/apple/).element.id).toBe('ex-regexp-selector');
-  })
+  describe('regexp selector', function () {
+    it('should work', function () {
+      expect(FQ(/apple/).element.id).toBe('ex-regexp-selector');
+    });
+  });
 
-  it('should select by CSS Selector', function () {
-    expect(FQ('.foo-btn').element.id).toBe('ex-css-selector');
-  })
+  describe('css selector', function () {
+    it('should work', function () {
+      expect(FQ('.foo-btn').element.id).toBe('ex-css-selector');
+    });
+  });
 
-  it('should select by Ordered Selector', function () {
-    expect(FQ(/mango/, /banana/).element.id).toBe('ex-ordered-selector');
-  })
+  describe('ordered selector', function () {
+    it('should work', function () {
+      expect(FQ(/mango/, /banana/).element.id).toBe('ex-ordered-selector');
+    });
 
-  it('should select by Ordered Selector with index', function () {
-    expect(FQ(/plum/, 'a', /orange/, 1).element.id).toBe('ex-index-ordered-selector');
-  })
+    it('should work by index', function () {
+      expect(FQ(/plum/, 'a', /orange/, 1).element.id).toBe('ex-index-ordered-selector');
+    });
 
-  it('should select last element in first selector', function () {
-    expect(FQ('a', /grape/, 0).element.id).toBe('ex-first-selector-last-element');
-  })
+    it('should select children of element selected by previous selector', function () {
+      expect(FQ('.fruits-1', /melon/).element.id).toBe('ex-search-children');
+    });
+  });
 
-  it('should select first element in not first selector', function () {
-    expect(FQ(/guava/, 'button').element.id).toBe('ex-other-selector-first-element');
-  })
+  describe('selecting priority', function () {
+    it('should select last element in first selector', function () {
+      expect(FQ('a', /grape/, 0).element.id).toBe('ex-first-selector-last-element');
+    });
 
-  it('should select children of element selected by previous selector', function () {
-    expect(FQ('.fruits-1', /melon/).element.id).toBe('ex-search-children');
-  })
+    it('should select first element in not first selector', function () {
+      expect(FQ(/guava/, 'button').element.id).toBe('ex-other-selector-first-element');
+    });
+  });
 
-  it('should select by table selector', function () {
-    expect(FQ({col: /head2/, row: /value1/}, 'input').element.id).toBe('ex-table-selector');
-  })
+  describe('table selector', function () {
+    it('should work', function () {
+      expect(FQ({col: /head2/, row: /value1/}, 'input').element.id).toBe('ex-table-selector');
+    });
 
-  it('should select by table selector with row index', function () {
-    expect(FQ({col: /head2/, row: 1}, 'input').element.id).toBe('ex-table-selector');
-  })
+    it('should work by row index', function () {
+      expect(FQ({col: /head2/, row: 1}, 'input').element.id).toBe('ex-table-selector');
+    });
 
-  it('should take colspan to use table selector', function () {
-    expect(FQ({col: /row2/, row: 3}).text()).toBe('cell12');
-  })
+    it('should work with colspan table', function () {
+      expect(FQ({col: /row2/, row: 3}).text()).toBe('cell12');
+    });
 
-  it('should take rowspan to use table selector', function () {
-    expect(FQ({col: /row8/, row: /cell21/}).text()).toBe('cell28');
-  })
+    it('should work with rowspan table ', function () {
+      expect(FQ({col: /row8/, row: /cell21/}).text()).toBe('cell28');
+    });
 
-  it('should ignore cell not displayed to use table selector', function () {
-    expect(FQ({col: /row9/, row: /cell11/}).text()).toBe('cell19');
-  })
+    it('should ignore cell not displayed', function () {
+      expect(FQ({col: /row9/, row: /cell11/}).text()).toBe('cell19');
+    });
+  });
 
-  it('should select by regexp', function () {
-    FQ(/Selections:/).select('do.*');
-    expect(document.querySelector('#ex-selections').value).toBe('animal');
-    expect(document.querySelector('#ex-select-by-regexp').selected).toBeTruthy();
-  })
+  describe('select action', function () {
+    it('should work by regexp', function () {
+      FQ(/Selections:/).select('do.*');
+      expect(document.querySelector('#ex-selections').value).toBe('animal');
+      expect(document.querySelector('#ex-select-by-regexp').selected).toBeTruthy();
+    });
 
-  it('should select option having same value', function () {
-    FQ(/Selections:/).select('durian');
-    expect(document.querySelector('#ex-selections').value).toBe('fruits');
-    expect(document.querySelector('#ex-select-same-value').selected).toBeTruthy();
-  })
+    it('should work with option having same value', function () {
+      FQ(/Selections:/).select('durian');
+      expect(document.querySelector('#ex-selections').value).toBe('fruits');
+      expect(document.querySelector('#ex-select-same-value').selected).toBeTruthy();
+    });
+  });
 
   describe('heading selector', function () {
-    it('should select by heading selector', function () {
+    it('should work', function () {
       expect(FQ({ heading: /sky/ }, /foo/).element.id).toBe('ex-heading-selector');
       expect(FQ({ heading: /sky/ }, /foo/, /foo/)).toBe(null);
     });
@@ -89,7 +101,7 @@ describe('template', function() {
       expect(FQ({ heading: /ruby/ }, /foo/, /foo/)).not.toBe(null);
     });
 
-    it('should select by nested heading selector', function () {
+    it('should work by nested heading selector', function () {
       expect(FQ({ heading: /class A/ }, { heading: /Ken/ }, /male/).element.id).toBe(
         'ex-nested-heading-selector'
       );
@@ -100,7 +112,7 @@ describe('template', function() {
   });
 
   describe('group member selector', function () {
-    it('should select by group member selector', function () {
+    it('should work', function () {
       expect(FQ({ group: '.colors', member: /sea/}, /foo/).element.id).toBe(
         'ex-group-member-selector'
       );
@@ -113,7 +125,7 @@ describe('template', function() {
       );
     });
 
-    it('should select by nested group member selector', function () {
+    it('should work by nested group member selector', function () {
       expect(
         FQ(
           { group: '.classroom', member: /class A/ },
